0066e9ffbfc9a42074cd4242ec632a51cadf8d52
wang.heng <wang.heng@immomo.com>
2015-04-27 10:01:46 +0800

    12生肖deomo

--- ChineseZodiac/ChineseZodiac/ViewController.swift
+++ ChineseZodiac/ChineseZodiac/ViewController.swift
@@ -14,12 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBOutlet weak var yearNum: UITextField!
 
+    @IBOutlet weak var imageObj: UIImageView!
+    
+    let offset = 3
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func touchesEnded(touches: Set<NSObject>, withEvent event: UIEvent) {
+        yearNum.resignFirstResponder()
+    }
 
+    @IBAction func goConfirm(sender: AnyObject) {
+//        yearNum.resignFirstResponder()
+        if let year = yearNum.text.toInt() {
+            var num = (year - offset) % 12
+            imageObj.image = UIImage(named: String(num))
+        }
+    }
 
 }
 
